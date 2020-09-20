3e5f37b7a34ae738275d2ef511c63b7d974b6e73
ashler24 <43813305+ashler24@users.noreply.github.com>
2020-09-20 22:39:58 +0530

    added mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/urlShortener',{
+    useNewUrlParser:true, useUnifiedTopology:true
+})
 
 app.set('view engine','ejs');
 
@@ -8,9 +13,15 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); /
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
+
+
 app.get('/',(req,res) =>{
     res.render('index')
 })
+
+app.post('/shortUrls', (req,res) =>{
+
+});
 
 app.listen(PORT,()=>{
     console.log('Server listening on port '+PORT);
