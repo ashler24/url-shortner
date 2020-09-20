e45e4e67096e6afec4a9fcc8688efc2213e9e6a0
ashler24 <43813305+ashler24@users.noreply.github.com>
2020-09-20 21:34:36 +0530

    created server.js and index.ejs

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.set('view engine','ejs');
+
+app.get('/',(req,res) =>{
+    res.render('index')
+})
+
+app.listen(PORT,()=>{
+    console.log('Server listening on port '+PORT);
+});
