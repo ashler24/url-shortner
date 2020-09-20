c26febf63b6e4d913d5e0a06439a09c62fcea639
ashler24 <43813305+ashler24@users.noreply.github.com>
2020-09-20 22:22:56 +0530

    added table and shrink button

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 5000;
 
 app.set('view engine','ejs');
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.get('/',(req,res) =>{
     res.render('index')
 })
